6bc999de7f25327b7ebcd23dbc9ffdc141c26ef6
arielmarno <arielmarno@outlook.com>
2025-07-18 16:59:34 -0300

    feat: actualizar metadatos y agregar imágenes de fondo

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["amber-cognitive-citizenship-concerned.trycloudflare.com"],
+    allowedHosts: [""],
   },
 })
